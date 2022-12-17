65a0360fcce0f802394e7c2ffe0d40210ac456e7
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-12-17 15:10:52 +0300

    admin panel complete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from flask_login import login_required
 import numpy as np
 import joblib
 import pickle 
@@ -15,6 +16,7 @@ import re
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import session as Childs_name
 scaler =StandardScaler()
 model = pickle.load(open("model.pkl", 'rb'))
 
@@ -46,6 +48,7 @@ def signup():
         Last_name = request.form['Last_name']
         Email = request.form['Email']
         Password = request.form['Password'].encode('utf-8')
+        Childs_name = request.form['Childs_name']
         hash_password = bcrypt.generate_password_hash(Password)
     
         cur = mysql.connection.cursor()
@@ -60,11 +63,12 @@ def signup():
             flash('Please fill out the form')
         else:
             cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO user(First_name,Last_name,Email,Password) VALUES (%s,%s,%s,%s)",(First_name,Last_name,Email,hash_password,))
+            cur.execute("INSERT INTO user(First_name,Last_name,Email,Password,Childs_name) VALUES (%s,%s,%s,%s,%s)",(First_name,Last_name,Email,hash_password,Childs_name))
             mysql.connection.commit()
             session['First_name'] = First_name
             session['Last_name']=Last_name
             session['Email']=Email
+            session['Childs_name']=Childs_name
             cur.close()
            
             flash("Registerd successfully")
@@ -75,7 +79,7 @@ def signup():
     
 @app.route('/login', methods=["GET","POST"])
 def login():
-    msg=''
+   
     
     if request.method == 'POST':
         Email = request.form['Email']
@@ -96,7 +100,8 @@ def login():
                 session['First_name'] = users['First_name']
                 session['Last_name'] = users['Last_name']
                 session['Email'] = users['Email']
-                msg =" You have logged in successfully"
+              
+                flash(" You have logged in successfully")
                 return redirect(url_for("index"))
                 cur.close()
             else:
@@ -133,17 +138,22 @@ def index1():
         A2 = int(request.form['A2'])
     
         feature = ([[A9,A8,A6,A4,A3,A2,]])
-    
-       
         prediction = model.predict(feature)
-     
+        
+        cur = mysql.connection.cursor()
+        result=cur.execute("SELECT * FROM user ")
+        result=cur.fetchone()
+        session['Childs_name']=result['Childs_name']
+        name=session['Childs_name']
+  
         if prediction==1:
-            prediction = "The child has a high risk of Autism" 
+            prediction = "has a high risk of Autism" 
         else:
-            prediction = "The child has a low risk of Autism" 
+            prediction = "has a low risk of Autism" 
     
     
-        return render_template("asd_result.html", prediction_text = "{}".format(prediction))
+        return render_template("asd_result.html", prediction_text = "{} {}".format(name,prediction))
+    
     else:
         return render_template("index.html")
 
@@ -153,12 +163,81 @@ def profile():
   
     # Check if user is loggedin
     if 'loggedin' in session:
-        cur.execute('SELECT * FROM users WHERE Email = %s', [session['Email']])
+        cur.execute('SELECT * FROM user WHERE Email = %s', [session['Email']])
         account = cur.fetchone()
         # Show the profile page with account info
         return render_template('index.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+@app.route('/admin', methods=['GET','POST'])
+def admin():
+    #if 'logged in' in session:
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM user")
+    users = cur.fetchall()
+    return render_template("admin.html",users=users)
+    #return redirect(url_for('index'))
+    
+    
+@app.route("/view",methods=['GET','POST'])
+def view():
+    viewuserId=request.args.get('User_id')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM user WHERE User_id=%s",(viewuserId,))
+    user = cur.fetchone()
+    return render_template("view.html",user=user)
+
+@app.route("/password_change",methods=['GET','POST'])
+def password_change():
+    message=''
+    changePassUserid =request.args.get('User_id')
+    if request.method == 'POST' and 'Password' in request.form and 'User_id' in request.form:
+        Password = request.form['Password']
+        User_id = request.form['User_id']
+        if not Password:
+            message ='Please fill out the form!'
+        else:
+            cur = mysql.connection.cursor()
+            cur.execute('UPDATE user SET Password=%s WHERE User_id=%s',(Password,(User_id,)))
+            mysql.connection.commit()
+            message ='Password updated!'
+    elif request.method == 'POST':
+        message ='Password updated!'
+    return render_template("password_change.html",message =message, changePassUserid =changePassUserid)
+
+            
+@app.route("/edit",methods=['GET','POST'])
+def edit():
+    message=''
+    editUserid =request.args.get('User_id')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM user WHERE User_id=%s",(editUserid,))
+    editUser =cur.fetchone()
+    if request.method == 'POST' and 'First_name' in request.form and 'Last_name' in request.form and 'User_id' in request.form and 'Email' in request.form:
+        First_name = request.form['First_name']
+        Last_name = request.form['Last_name']
+        User_id = request.form['User_id']
+        Email = request.form['Email']
+        """
+        if not re.match(r'[A-Za-z0-9]+',First_name):
+            message ='name must contain characters and numbers!'
+        else:
+        """
+        cur.execute('UPDATE user SET First_name=%s, Last_name=%s, Email=%s, WHERE User_id=%s',(First_name,Last_name,Email(User_id,),))
+        mysql.connection.commit()
+        message ='User updated!'
+        return redirect(url_for('admin'))
+    elif request.method == 'POST':
+        msg= 'Please fill out the form!'
+    return render_template("edit.html",msg=msg,editUser=editUser)
+            
+@app.route("/delete",methods=['GET']) 
+def delete():
+    deleteUserId =request.args.get('User_id') 
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM user WHERE User_id=%s',(deleteUserId,))
+    mysql.connection.commit()
+    return redirect(url_for('admin'))
 
 if __name__ == "__main__":
     app.debug =True
